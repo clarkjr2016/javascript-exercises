32bbafb2f356955e3e3cef3f21e3f921adb63e93
Eugene Clark <clarkjr2016@gmail.com>
2022-08-31 19:25:22 -0400

    Completed folders 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(s,n) {
+let string = "";
+
+if (n < 0) {
+string = "ERROR";
+
+} else {
+for (let i = n; i > 0; i--) {
+    string += s.slice();
+        }
+} 
+return string
+
 
 };
+
+repeatString("hello", 0);
+
 
 // Do not edit below this line
 module.exports = repeatString;
