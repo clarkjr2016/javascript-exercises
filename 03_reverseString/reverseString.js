e819a8af7636cd257e73ddd8ddedfe734bfb73ec
Eugene Clark <clarkjr2016@gmail.com>
2022-09-05 23:05:50 -0400

    In stage 3 of the problem solving process for 03 folder

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(s) {
+
+    const inputArray = s.split("");
+/* trying to figure out how to store the popped elements of 
+inputArray into an array called reverseString
+*/
+  return reverseArray;
+
 
 };
+
+reverseString("dog")
 
 // Do not edit below this line
 module.exports = reverseString;
