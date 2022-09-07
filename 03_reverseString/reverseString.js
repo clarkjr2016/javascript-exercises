c1fd4321ce623257b2a2b37416cf32706ddda8e4
Eugene Clark <clarkjr2016@gmail.com>
2022-09-07 19:04:23 -0400

    Finished 03 exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,17 @@
 const reverseString = function(s) {
 
     const inputArray = s.split("");
+    inputArray.reverse();
+   const reverseOutput = inputArray.join("");
 /* trying to figure out how to store the popped elements of 
 inputArray into an array called reverseString
 */
-  return reverseArray;
+return reverseOutput;
 
 
 };
 
-reverseString("dog")
+reverseString("Hello my name is Eugene")
 
 // Do not edit below this line
 module.exports = reverseString;
