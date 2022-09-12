127be6e8cfcdfb7e965428b39e75068cdab86e23
Eugene Clark <clarkjr2016@gmail.com>
2022-09-11 20:38:05 -0400

    Completed challenge 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(y) {
+let year = y;
+let result;
 
+if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 == 0)) {
+    result = true;
+}else {
+    result = false;
+}
+
+
+return result;
 };
 
+leapYears(700);
 // Do not edit below this line
 module.exports = leapYears;
