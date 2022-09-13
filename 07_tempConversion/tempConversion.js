c5a787a43ce36f7abb2097fe8ca5f1073919aa8e
Eugene Clark <clarkjr2016@gmail.com>
2022-09-12 20:58:27 -0400

    Completed Challenge 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+let conversion = (temperature - 32) * .555556
+let rounded = Math.round(conversion * 10) / 10
+return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let conversion = temperature * 1.8 + 32;
+  let rounded = Math.round(conversion * 10) / 10
+  return rounded;
 };
+
 
 // Do not edit below this line
 module.exports = {
