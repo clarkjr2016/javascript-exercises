496f7403b5755cb5615806f270e388ef24dfa39a
Eugene Clark <clarkjr2016@gmail.com>
2022-09-13 21:02:40 -0400

    You should be proud of yourself for figuring out why the arguments objet was returning with a length of 1 instead of 0

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,14 +1,20 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
-};
+const sum = function(a) {
+if (a.length === 0) {
+  sum1 = 0;
+} else {
+ sum1 = a.reduce((a,b) => {return a + b})
+}
+   return sum1;
+  };
+
 
 const multiply = function() {
 
@@ -21,6 +27,8 @@ const power = function() {
 const factorial = function() {
 	
 };
+
+sum([2,4]);
 
 // Do not edit below this line
 module.exports = {
