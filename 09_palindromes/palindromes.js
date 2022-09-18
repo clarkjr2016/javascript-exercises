20952b7fa0e8eac6b8ecca8353dad0ae5fc2c9d3
Eugene Clark <clarkjr2016@gmail.com>
2022-09-17 21:06:20 -0400

    Halfway done with challenge 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reverse;
+    let array = word.split("");
+    let reversal = array.reverse();
+    for (const w of reversal) {
+        if (/[a-zA-Z]/.test(w) === false) {
+           const index = reversal.indexOf(w);
+          let nonWord = reversal.splice(index,1)
+          
+          reversal.push(nonWord);
+        } else {
+            continue;
+        }
+    }
+    reverse = reversal.join("")
+    let finalValue;
+    if (reverse === word) {
+        finalValue = true;
+    } else {
+        finalValue = false;
+    }
 
+    return finalValue;
 };
+
+palindromes("racecar!");
 
 // Do not edit below this line
 module.exports = palindromes;
