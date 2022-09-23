13b0852df71eaff369a290976086a9abcdf65a5a
Eugene Clark <clarkjr2016@gmail.com>
2022-09-23 05:06:55 -0400

    I need HELP

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+let array = [1,1];
+for (i = 2; array.length < n; i++) {
+     if (n == 1) {
+        return array;
+    }
+    else if (n > 1) {
+        let sum = array[i - 1] + array[i - 2];
+        array.push(sum);
+        return array;
+    } else if (n < 0 || Math.sign(n) == -1) {
+        array = ["OOPS"];
+        return array;
+    }
+    
+}
+return array;
 };
+
+fibonacci(-1)
 
 // Do not edit below this line
 module.exports = fibonacci;
