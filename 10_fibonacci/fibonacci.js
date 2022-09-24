785f4f8f295362ef9d4bd2e86b7e520247e4ef2d
Eugene Clark <clarkjr2016@gmail.com>
2022-09-24 01:55:37 -0400

    Completd fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = function(n) {
 let array = [1,1];
+if (n < 0) {
+    array = ["OOPS"];
+}
+
 for (i = 2; array.length < n; i++) {
      if (n == 1) {
         return array;
@@ -7,17 +11,15 @@ for (i = 2; array.length < n; i++) {
     else if (n > 1) {
         let sum = array[i - 1] + array[i - 2];
         array.push(sum);
-        return array;
-    } else if (n < 0 || Math.sign(n) == -1) {
-        array = ["OOPS"];
-        return array;
-    }
+        
+    } 
     
 }
+
 return array.pop();
 };
 
-fibonacci(-1)
+fibonacci(4)
 
 // Do not edit below this line
 module.exports = fibonacci;
