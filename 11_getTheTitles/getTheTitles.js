998042c9665c8d0bd0777dd7121894499199a3f6
Eugene Clark <clarkjr2016@gmail.com>
2022-09-24 13:17:26 -0400

    I am him and he is me. I am Himothy Hendrix

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
+const getTheTitles = function(a) {
+let titles = [];
 
-};
+a.forEach(element => {
+    let index = a.indexOf(element);
+    let title = a[index].title;
+    titles.push(title);
+    
+});
+return titles;
+}
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
