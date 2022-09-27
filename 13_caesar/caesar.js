d2e74dcae6e1a4d32306fd39a68024bd3d0309c0
Eugene Clark <clarkjr2016@gmail.com>
2022-09-26 21:14:54 -0400

    Making progress!

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,14 @@
-const caesar = function() {
+const caesar = function(s,n) {
 
+let stringArray = s.split("");
+let unicodeArray = stringArray.map(x => x.charCodeAt(0));
+let shiftedArray = unicodeArray.map(x => x + n);
+let converted = shiftedArray.map(x => String.fromCharCode(x))
+let complete = converted.join("")
+return complete;
 };
 
+
+caesar("Abc",1)
 // Do not edit below this line
 module.exports = caesar;
