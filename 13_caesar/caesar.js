71a75478d651904d6cc6b2b386609654de61ee5b
Eugene Clark <clarkjr2016@gmail.com>
2022-09-29 04:52:19 -0400

    What is the UNicode of W?

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -2,13 +2,18 @@ const caesar = function(s,n) {
 
 let stringArray = s.split("");
 let unicodeArray = stringArray.map(x => x.charCodeAt(0));
-let shiftedArray = unicodeArray.map(x => x + n);
+let shiftedArray = unicodeArray.map(x => {if (/^[a-z]/i.test(String.fromCharCode(x))) {
+    return x + n;
+} else {
+    return x;
+}
+});
 let converted = shiftedArray.map(x => String.fromCharCode(x))
 let complete = converted.join("")
 return complete;
 };
 
 
-caesar("Abc",1)
+console.log(caesar("Hello, World!",5))
 // Do not edit below this line
 module.exports = caesar;
