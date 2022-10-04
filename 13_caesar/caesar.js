3114f8cb2ea4ec11b38eb98dab87ac6f7fecd667
Eugene Clark <clarkjr2016@gmail.com>
2022-10-03 21:46:53 -0400

    Not my proudest moment but I'll review this code later

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,19 +1,41 @@
-const caesar = function(s,n) {
+const caesar = function(str ,amount) {
 
-let stringArray = s.split("");
-let unicodeArray = stringArray.map(x => x.charCodeAt(0));
-let shiftedArray = unicodeArray.map(x => {if (/^[a-z]/i.test(String.fromCharCode(x))) {
-    return x + n;
-} else {
-    return x;
+  // Wrap the amount
+  if (amount < 0) {
+    return caesar(str, amount + 26);
+  }
+
+  // Make an output variable
+  var output = "";
+
+  // Go through each character
+  for (var i = 0; i < str.length; i++) {
+    // Get the character we'll be appending
+    var c = str[i];
+
+    // If it's a letter...
+    if (c.match(/[a-z]/i)) {
+      // Get its code
+      var code = str.charCodeAt(i);
+
+      // Uppercase letters
+      if (code >= 65 && code <= 90) {
+        c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+      }
+
+      // Lowercase letters
+      else if (code >= 97 && code <= 122) {
+        c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+      }
+    }
+
+    // Append
+    output += c;
+  }
+
+  // All done!
+  return output;
 }
-});
-let converted = shiftedArray.map(x => String.fromCharCode(x))
-let complete = converted.join("")
-return complete;
-};
-
-
 console.log(caesar("Hello, World!",5))
 // Do not edit below this line
 module.exports = caesar;
